bba62008c4f518355d6dc0c692cf9e8c1c2c3910
lza_menace <lza_menace@protonmail.com>
2021-02-09 15:54:10 -0800

    adding code to tally up results and render summary info in template

--- generate_report.py
+++ generate_report.py
@@ -5,12 +5,21 @@ from csv import reader
 
 def run():
     csv_data = {}
+    totals = {
+        'accounts': [],
+        'regions': [],
+        'scan_results': [],
+        'passes': [],
+        'fails': [],
+        'infos': []
+    }
 
     with open('template.html') as f:
         tpl = f.read()
 
     with open('prowler-test.csv') as csv_file:
         csv_reader = reader(csv_file, delimiter=',')
+        next(csv_reader, None)
         for idx, row in enumerate(csv_reader):
             csv_data[idx] = {
                 'profile': row[0],
@@ -27,8 +36,26 @@ def run():
                 'service_name': row[11],
             }
 
+            d = csv_data[idx]
+            t = totals
+
+            if d['account_id'] not in t['accounts']:
+                t['accounts'].append(d['account_id'])
+
+            if d['region'] not in t['regions']:
+                t['regions'].append(d['region'])
+
+            t['scan_results'].append(idx)
+
+            if d['result'] == 'PASS':
+                t['passes'].append(idx)
+            elif d['result'] == 'FAIL':
+                t['fails'].append(idx)
+            elif d['result'] == 'INFO':
+                t['infos'].append(idx)
+
     t = Template(tpl)
-    rendered = t.render(data=csv_data)
+    rendered = t.render(data=csv_data, totals=totals)
 
     with open('report.html', 'w') as f:
         f.write(rendered)
