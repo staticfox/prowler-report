d3c345160a04870f8d965a923672675758b846d4
Matt <static@staticfox.net>
2021-02-11 10:46:26 -0800

    wip / ideas, requires 3.7+

--- generate_report.py
+++ generate_report.py
@@ -1,68 +1,85 @@
 from jinja2 import Template
 from os import path
 from csv import reader
+from typing import NamedTuple
 
+
+class ProwlerTestResult(NamedTuple):
+    profile: str
+    account_id: str
+    region: str
+    title_id: str
+    result: str
+    scored: str
+    level: str
+    title_text: str
+    notes: str
+    compliance: str
+    severity: str
+    service_name: str
+
+
+class Total:
+    def __init__(self, results):
+        self._results = results
+        self._by_sev = {}
+
+        self._parse()
+
+    def _parse(self):
+        for r in self._results:
+            self._by_sev.setdefault(r.severity.lower(), 0)
+            self._by_sev[r.severity.lower()] += 1
+
+    @property
+    def accounts(self):
+        return set([r.account_id for r in self._results])
+
+    @property
+    def regions(self):
+        return set([r.region for r in self._results])
+
+    @property
+    def scan_results(self):
+        return len(self._results)
+
+    @property
+    def passes(self):
+        return self._by_result('PASS')
+
+    @property
+    def fails(self):
+        return self._by_result('FAIL')
+
+    @property
+    def infos(self):
+        return self._by_result('INFO')
+
+    def _by_result(self, result: str):
+        # NOTE: Don't use a generator since the caller may want to
+        #       inspect the size of the dataset.
+        return [r for r in self._results if r.result == result]
+
+    @property
+    def all_by_sev(self):
+        return self._by_sev
+
+    @property
+    def passes_by_sev(self):
+        return self._by_sev['PASS']  # idk what this has
+
+    def __iter__(self):
+        yield next(self._results)
 
 def run():
-    csv_data = {}
-    totals = {
-        'accounts': [],
-        'regions': [],
-        'scan_results': [],
-        'passes': [],
-        'fails': [],
-        'infos': [],
-        'all_by_sev': {},
-        'passes_by_sev': {},
-        'fails_by_sev': {},
-        'infos_by_sev': {},
-    }
-
     with open('template.html') as f:
         tpl = f.read()
 
     with open('prowler-test.csv') as csv_file:
         csv_reader = reader(csv_file, delimiter=',')
         next(csv_reader, None)
-        for idx, row in enumerate(csv_reader):
-            csv_data[idx] = {
-                'profile': row[0],
-                'account_id': row[1],
-                'region':  row[2],
-                'title_id':  row[3],
-                'result': row[4],
-                'scored': row[5],
-                'level':  row[6],
-                'title_text':  row[7],
-                'notes': row[8],
-                'compliance': row[9],
-                'severity': row[10],
-                'service_name': row[11],
-            }
-
-            d = csv_data[idx]
-            t = totals
-
-            if d['account_id'] not in t['accounts']:
-                t['accounts'].append(d['account_id'])
-
-            if d['region'] not in t['regions']:
-                t['regions'].append(d['region'])
-
-            t['scan_results'].append(idx)
-
-            if d['result'] == 'PASS':
-                t['passes'].append(idx)
-            elif d['result'] == 'FAIL':
-                t['fails'].append(idx)
-            elif d['result'] == 'INFO':
-                t['infos'].append(idx)
-
-            l = d['severity'].lower()
-            if l not in t['all_by_sev']:
-                t['all_by_sev'][l] = list()
-            if idx not in t['all_by_sev'][l]:
-                t['all_by_sev'][l].append(idx)
+        totals = Total(list(map(ProwlerTestResult._make, csv_reader)))
+        csv_data = total._results
 
     tpl = Template(tpl)
     rendered = tpl.render(data=csv_data, totals=totals)
